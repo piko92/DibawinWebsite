9fca47d022c6317f21b572f2741aa2c630e6ac9c
Panahi <panahi92.sp@gmail.com>
2019-11-06 17:10:08 +0330

    Add project files.

---
+++ DibawinWebsite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DibawinWebsite.Areas.Identity.Data;
+using DibawinWebsite.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DibawinWebsite.Areas.Identity.IdentityHostingStartup))]
+namespace DibawinWebsite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DibawinWebsiteContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DibawinWebsiteContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>()
+                    .AddEntityFrameworkStores<DibawinWebsiteContext>();
+            });
+        }
+    }
+}
---
+++ DibawinWebsite/wwwroot/shared/shared_contents/font/fontawesome-pro-5.0.10-ulabs/fontawesome-pro-5.0.10/README.md
@@ -0,0 +1,8 @@
+# Font Awesome 5.0.10
+
+Thanks for downloading Font Awesome! We're so excited you're here.
+
+Our documentation is available online. Just head here:
+
+https://fontawesome.com
+https://www.ulabs.ir
